84f822bcea1fbe2a641a9814f46ea799662cc483
caojen <136451549@qq.com>
2019-08-01 18:42:45 +0800

    add changes at 18:42

--- DarkHub/scripts/sign.js
+++ DarkHub/scripts/sign.js
@@ -1,22 +1,31 @@
-document.write("<script src='./vue.js'></script>");
+document.write("<script src='./jquery.js></script>");
 
-function getUserTextInfo() {
-    var username;
-    var userpass;
+// function getUserTextInfo() {
+//     var username;
+//     var userpass;
 
-    username = document.getElementById('userName');
-    userpass = document.getElementById('userPass');
+//     username = document.getElementById('userName');
+//     userpass = document.getElementById('userPass');
 
-    alert('hah');
-}
+//     alert('hah');
+// }
 
-function getUserIcon() {//登录时头像，获取username时马上显示
+// function getUserIcon() {//登录时头像，获取username时马上显示
     
-}
+// }
+
+// function signIn(){
+//     alert("signin");
+// }
+// function signup(){
+//     alert("signup");
+// }
 
 function signIn(){
-    alert("signin");
+    let username = $("#userName").value();
+    let password = $("#userPass").value();
+    alert(username + ' ' + password);
 }
-function signup(){
-    alert("signup");
+function signUp(){
+    alert("not completed.")
 }
