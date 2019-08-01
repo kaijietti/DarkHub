8eee9a55a251b9ca0eecae84e4fbe26ddb3fb94c
kaijietti <43436010+kaijietti@users.noreply.github.com>
2019-08-01 15:13:41 +0800

    Add files via upload

---
+++ DarkHub/scripts/sign.js
@@ -0,0 +1,13 @@
+function getUserTextInfo() {
+    var username;
+    var userpass;
+
+    username = document.getElementById('userName');
+    userpass = document.getElementById('userNmae');
+
+    alert('hah');
+}
+
+function getUserIcon() {//登录时头像，获取username时马上显示
+    
+}
