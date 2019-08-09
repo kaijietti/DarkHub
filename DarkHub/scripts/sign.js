1f2e45f385d2bbf1785824bf47887828c5f73a6f
caojen <136451549@qq.com>
2019-08-09 22:09:08 +0800

    debug

--- DarkHub/scripts/sign.js
+++ DarkHub/scripts/sign.js
@@ -118,6 +118,7 @@ function Login(){
       setCookie(username,1);
       window.location.href = './index';
     }else{
+      alert('Wrong username or password!');
       $('#password_login').val('');
     }
   });
